f909fa7100dd0007df22f5ab87530782ffb61bcd
Marcos Wenneton Araújo <wenneton@outlook.com>
2019-04-09 11:59:34 -0400

    Estrutura do backend

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,20 @@
+const Box = require('../models/Box');
+
+class BoxController {
+    async store(req, res) {
+        const box = await Box.create(req.body);
+
+        return res.json(box);
+    }
+
+    async show(req, res) {
+        const boxes = await Box.findById(req.params.id).populate({
+            path: "files",
+            options: { sort: { createdAt: -1 } } // 1: crescente, -1: decrescente
+        }); // populate show related schemas
+
+        return res.json(boxes);
+    }
+}
+
+module.exports = new BoxController();
---
+++ src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+// Separação de usuários com websocket (teste após a conclusão do front)
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    });
+})
+
+// Conexão com o BD
+mongoose.connect('mongodb+srv://omnistack:omnistack@clusteromnistack-bpc3p.mongodb.net/omnistack?retryWrites=true',
+    {
+        useNewUrlParser: true
+    }
+);
+
+// Middleware para se conectar a uma room
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+app.listen(3001);
